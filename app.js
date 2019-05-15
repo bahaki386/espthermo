61951f1576ba534a21a178326dadffe4d6867e11
Haruki Baba <harukibaba262@hotmail.co.jp>
2019-05-15 15:51:04 +0900

    lint

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var bodyParser=require('body-parser');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors')
+let express = require('express')
+let path = require('path')
+let bodyParser = require('body-parser')
+let cookieParser = require('cookie-parser')
+let logger = require('morgan')
 
-var indexRouter = require('./routes/index');
+let indexRouter = require('./routes/index')
 
-var app = express();
+let app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
+app.use('/', indexRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
-let express = require('express');
-let router = express.Router();
-let temp=0;
+let express = require('express')
+let router = express.Router()
+let temp = 0
 /* GET home page. */
-router.get('/', (req, res, next) =>{
+router.get('/', (req, res, next) => {
   console.log(temp)
-  res.render('index', { title: '遠隔温度計',temp: temp });
-});
+  res.render('index', { title: '遠隔温度計', temp: temp })
+})
 
-router.post('/',(req,res,next)=>{
-  temp=req.body.temp;
-  console.log(req.body);
-  console.log(req.body.temp);
-  console.log(temp);
-  res.end();
-});
+router.post('/', (req, res, next) => {
+  temp = req.body.temp
+  console.log(req.body)
+  console.log(req.body.temp)
+  console.log(temp)
+  res.end()
+})
 
-module.exports = router;
+module.exports = router
