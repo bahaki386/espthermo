5ae519ac2a469068f88588ffa1c9363a51ef41d8
Haruki Baba <harukibaba262@hotmail.co.jp>
2019-05-15 14:21:40 +0900

    server

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
-
+let express = require('express');
+let router = express.Router();
+let temp=0;
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) =>{
+  console.log(temp)
+  res.render('index', { title: '遠隔温度計',temp: temp });
+});
+
+router.post('/',(req,res,next)=>{
+  temp=req.body.temp;
+  console.log(req.body);
+  console.log(req.body.temp);
+  console.log(temp);
+  res.end();
 });
 
 module.exports = router;
